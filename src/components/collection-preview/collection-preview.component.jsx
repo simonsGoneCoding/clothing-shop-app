432c6fb23d6cc2a61548c7bb2e36d5c0accde098
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-24 22:30:38 +0100

    updating app with collection preview and collection item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import CollectionItem from "../collection-item/collection-item.component";
+
 import "./collection-preview.style.scss";
 
 const CollectionPreview = ({ title, items }) => {
@@ -8,8 +11,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map(item => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otheItemProps }) => (
+            <CollectionItem key={id} {...otheItemProps} />
           ))}
       </div>
     </div>
