f4a0f668d428baf0a3cfa6ee445a1034e757805c
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-24 21:28:15 +0100

    adding header component and links to home, shop page and contact

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ const CollectionPreview = ({ title, items }) => {
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
-          .filter((item, idx) => idx < 4)
+          .filter((item, index) => index < 4)
           .map(item => (
             <div key={item.id}>{item.name}</div>
           ))}
