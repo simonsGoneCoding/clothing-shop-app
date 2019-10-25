39de3d30e9e5948cdb3b672872f741b169351fa9
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-25 20:41:55 +0100

    updating app with sign-in-and-sign-up page and sign-in component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopPage/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSingUpPage from ".//pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/Shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSingUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+import "./sign-in.styles.scss";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      email: "",
+      password: ""
+    });
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div className="sing-in">
+        <h2>I already have an account</h2>
+        <span>Sing in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            required
+            onChange={this.handleInputChange}
+          />
+          <label>Email</label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            required
+            onChange={this.handleInputChange}
+          />
+          <label>Password</label>
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
