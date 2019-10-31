95287a20b1a15273d30dba37fff17e7deafd1d74
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-31 21:31:16 +0000

    fully integrated firebase goolge sing in and out into application

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,10 +54,12 @@ class SignIn extends Component {
             handleChange={this.handleChange}
             label="password"
           />
-          <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
